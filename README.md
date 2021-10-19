c121d18b71716d4bd06a650b3f608ce5e13995f6
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2021-10-19 12:12:43 +0200

    update README.md and dependencies versions

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
-# hotello:accounts-keycloak
+# eole:accounts-keycloak
 
 A login service for Keycloak.
+
+Forked from hotello:acccounts-keycloak :
+https://github.com/hotello/meteor-accounts-keycloak.git
 
 ## Configuration
 
 As any official Meteor account package, upsert a configuration object:
 
 ```js
-ServiceConfiguration.configurations.upsert({service: 'keycloak'}, {
-  $set: {
-    serverUrl: '<KEYCLOAK_SERVER_URL>',
-    realm: '<REALM_NAME>',
-    clientId: '<CLIENT_ID>',
-    realmPublicKey: '<PUBLIC_KEY>',
-    bearerOnly: true
-  }
-});
+ServiceConfiguration.configurations.upsert(
+  { service: 'keycloak' },
+  {
+    $set: {
+      serverUrl: '<KEYCLOAK_SERVER_URL>',
+      realm: '<REALM_NAME>',
+      clientId: '<CLIENT_ID>',
+      realmPublicKey: '<PUBLIC_KEY>',
+      bearerOnly: true,
+    },
+  },
+);
 ```
--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: "hotello:accounts-keycloak",
-  summary: "Login service for Keycloak accounts",
-  version: "2.0.0",
-  git: "https://github.com/hotello/meteor-accounts-keycloak.git"
+  name: 'eole:accounts-keycloak',
+  summary: 'Login service for Keycloak accounts',
+  version: '2.1.0',
+  git: 'https://github.com/eole/meteor-accounts-keycloak.git',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.5');
+Package.onUse(function (api) {
+  api.versionsFrom(['1.5', '2.3']);
 
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('hotello:keycloak-oauth@2.0.0');
-  api.imply('hotello:keycloak-oauth');
+  api.use('eole:keycloak-oauth@2.0.0');
+  api.imply('eole:keycloak-oauth');
 
-  api.addFiles("keycloak.js");
+  api.addFiles('keycloak.js');
 });
